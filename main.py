915608d6b4535d750213d11a571200b0e749e7cf
SimsumMC <79917043+SimsumMC@users.noreply.github.com>
2021-04-13 19:59:21 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import json
+import sys
+import traceback
+import os
+import discord
+from discord.ext import commands
+from commands.functions import get_prefix
+
+
+class CommunityBot(commands.Bot):
+
+    async def on_ready(self):
+        print(f'Der {self.user} wurde erfolgreich gestartet!')
+
+    async def on_message(self, message):
+        await self.process_commands(message)
+
+
+########################################################################################################################
+
+
+client = CommunityBot(command_prefix=commands.when_mentioned_or(get_prefix), help_command=None,
+                      intents=discord.Intents.all())
+
+for filename in os.listdir('./commands'):
+    if filename.endswith('.py'):
+        extension = f'commands.{filename[:-3]}'
+        try:
+            print(f'Der Command {extension} konnte erfolgreich geladen werden.')
+            client.load_extension(extension)
+        except Exception as e:
+            print(f'Der Command "{extension}" konnte nicht geladen werden.', file=sys.stderr)
+            traceback.print_exc()
+
+client.run('ODEwOTMzMTI0NTk4NTk1NjE0.YCq2Uw.NfBygn7JnrKMZ40nvVMlM748kyk')
