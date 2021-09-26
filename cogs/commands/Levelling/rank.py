1233fbc8344a0ca24f2a07dd3d02d5376b419f47
SimsumMC <79917043+SimsumMC@users.noreply.github.com>
2021-09-26 16:20:38 +0200

    added error when requested memmber is a bot

--- cogs/commands/Levelling/rank.py
+++ cogs/commands/Levelling/rank.py
@@ -5,8 +5,10 @@ from discord.ext import commands
 from discord.ext.commands import Bot, BucketType
 
 from cogs.core.config.config_botchannel import botchannel_check
+from cogs.core.config.config_embedcolour import get_embedcolour
 from cogs.core.config.config_levelling import get_rank_card
 from cogs.core.config.config_prefix import get_prefix_string
+from cogs.core.defaults.defaults_embed import get_embed_thumbnail, get_embed_footer
 from cogs.core.functions.logging import log
 
 
@@ -24,7 +26,19 @@ class rank(commands.Cog):
         user = ctx.author.name
         if not member:
             member = ctx.author
-        if member.bot:  # TODO : Error
+        if member.bot:
+            embed = discord.Embed(
+                title="**Fehler**",
+                description="Bots sind vom Levelsystem ausgeschlossen und verdienen daher keine XP!",
+                colour=await get_embedcolour(ctx.message),
+            )
+            embed._thumbnail = await get_embed_thumbnail()
+            embed._footer = await get_embed_footer(ctx)
+            await ctx.send(embed=embed)
+            await log(
+                text=f"{time}: Der Nutzer {user} hat versucht mit dem Befehl {await get_prefix_string(ctx.message)}rank das Level eines Bots einzusehen!",
+                guildid=ctx.guild.id,
+            )
             return
         file = discord.File(fp=await get_rank_card(member=member, guild=ctx.guild), filename="rank.png")
         await ctx.send(file=file)
